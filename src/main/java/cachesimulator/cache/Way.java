5e1a856951350a385642623bfeb0f7d5a150b3ee
Paulo Vinicius Lempek <161062793+Vilek72@users.noreply.github.com>
2025-07-23 12:14:37 -0300

    Update Way.java

--- src/main/java/cachesimulator/cache/Way.java
+++ src/main/java/cachesimulator/cache/Way.java
@@ -4,6 +4,15 @@ import cachesimulator.cache.enums.HitOrMiss;
 
 public class Way {
     private final Block block = new Block();
+    
+    public int getTag() {
+        return block.getTag();
+    }
+
+    public void clear() {
+        block.setValidityToOne(); // Reset validity to 0
+        block.setTag(0); // Reset tag to 0
+    }
 
     public boolean isFull() {
         return block.getValidity() == 1;
